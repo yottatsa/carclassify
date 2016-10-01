925ea1ec710a3737a6b50625999a1a91411f077e
Vladimir Eremin <veremin@mirantis.com>
2016-10-01 15:51:53 +0000

    image contrast and resize, model tune

--- classify.py
+++ classify.py
@@ -1,70 +1,77 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import glob
 import os
 import os.path
 import sys
-import glob
 
 import numpy as np
-import math
 import skimage.io
+from skimage import exposure, img_as_float, img_as_ubyte
 from skimage.draw import line
-from sklearn.externals import joblib
-from skimage.feature import hog
-
-
+from skimage.transform import rescale
 from sklearn import svm
-from skimage import exposure, img_as_float
+from sklearn.externals import joblib
+from sklearn.model_selection import GridSearchCV
+
 
 def adjust(img):
-    img = exposure.adjust_gamma(img, 2)
+    img = img_as_float(img)
     p2, p98 = np.percentile(img, (2, 98))
-    return exposure.rescale_intensity(img, in_range=(p2, p98))
+    img = exposure.rescale_intensity(img, in_range=(p2, p98))
+    return img_as_ubyte(img)
+
 
 def extract(img):
-    feature = adjust(img).flatten()
+    feature = rescale(img, 0.5)
+    feature = feature.flatten()
     return feature.tolist()
 
-def vector():
-    vec = []
-
-    def append(*args):
-        vec.append(args)
-        return vec
-    return append, vec
 
 def load_knowndata(filenames, shape):
-    vec, res = vector()
+    X = []
+    y = []
     for index, filename in enumerate(filenames):
         target = os.path.splitext(os.path.basename(filename))[0]
         target = int(target.split('-')[0])
         image = skimage.io.imread(filename)
         if image.shape == shape:
             print filename, target
-            res = vec(filename, target, extract(image))
-    names, targets, datas = zip(*res)
-    return names, np.array(targets), np.array(datas)
+            X.append(extract(adjust(image)))
+            y.append(target)
+    return np.array(X), np.array(y)
+
 
 def train(filenames, shape):
-    _, targets, datas = load_knowndata(filenames, shape)
-    classifier = svm.SVC(gamma=1e-8, decision_function_shape='ovo')
-    classifier.fit(datas, targets)
-    return classifier
+    Cs = np.logspace(-1, 3, 10)
+    gammas = np.logspace(-5, -1, 8)
+    svc = svm.SVC(decision_function_shape='ovo')
+    clf = GridSearchCV(estimator=svc, param_grid=dict(C=Cs, gamma=gammas),
+                       n_jobs=-1)
+    X, y = load_knowndata(filenames, shape)
+    clf.fit(X, y)
+    print clf.best_score_
+    print clf.best_estimator_.C
+    print clf.best_estimator_.gamma
+    return clf.best_estimator_
+
 
 def walk_image(image, shape, classifier):
     dx, dy = shape
-    vec, res = vector()
+    coords = []
+    X = []
     h, w = image.shape
-    for x in range(0, h, dx/10):
-        for y in range(0, w, dy/10):
+    for x in range(0, h, dx / 2):
+        for y in range(0, w, dy / 4):
             probe = image[x:x + dx, y:y + dy]
             if probe.shape == shape:
-                res = vec((x, y, dx, dy), probe, extract(probe))
+                coords.append((x, y, dx, dy))
+                X.append(extract(probe))
 
-    names, probes, datas = zip(*res)
-    predicted = classifier.predict(np.array(datas))
-    return names, predicted, probes
+    predicted = classifier.predict(np.array(X))
+    return coords, predicted
+
 
 def mark_region(image, x, y, h, w, mark=None):
     if not mark:
@@ -72,37 +79,23 @@ def mark_region(image, x, y, h, w, mark=None):
     image[line(x, y, x + h, y + w)] = mark
     image[line(x + h, y, x, y + w)] = mark
 
-def deduplicator():
-    hits = []
-    def func(x, y, r):
-        for lx, ly, lr in hits:
-            distance = math.sqrt((lx-x) * (lx-x) + (ly-y)*(ly-y))
-            if distance < r+lr:
-                return False
-        hits.append((x, y, r))	
-        return True
-    return func
 
 def predict(filenames, shape, classifier):
     for name in filenames:
-        show = deduplicator()
 
         result = os.path.join('result', os.path.basename(name))
         image = skimage.io.imread(name)
 
-        coords, predicted, probes = walk_image(image, shape, classifier)
         image = adjust(image)
+        coords, predicted = walk_image(image, shape, classifier)
 
-        for pred, coords, probe in zip(predicted, coords, probes):
-            if pred > 16:
-                x, y, dx, dy = coords
-                r = min(dx, dy)/3
-                if show(x+dx/2, y+dy/2, r):
-                    mark_region(image, x+5, y+5, dx-10, dy-10, mark=pred)
-                    #pr = os.path.join(
-                    #    'probe', 
-                    #    '{}-{}x{}-{}'.format(pred, x, y, os.path.basename(name)))
-                    #skimage.io.imsave(pr, probe)
+        marks = []
+        maxmark = 0
+        for coord, pred in zip(coords, predicted):
+            maxmark = max(maxmark, pred)
+            if pred > 64:
+                x, y, dx, dy = coord
+                mark_region(image, x + 10, y + 10, dx - 20, dy - 20, pred)
 
         skimage.io.imsave(result, image)
         print result
--- preclassify.sh
+++ preclassify.sh
@@ -5,11 +5,8 @@ shift
 for f
 do
 	b="$(basename "$f" .jpg)"
-	for x in 0 37 86 153 213 265 318 380 428 479 519 579
+	for x in 225x38
 	do
-		convert "$f" -crop 50x40+${x}+36 -colorspace Gray "$d/0-${b}-${x}.jpg"
-		convert "$f" -crop 50x40+$((x+10))+10 -colorspace Gray "$d/2-${b}-$((x+10)).jpg"
-		convert "$f" -crop 50x40+$((x+20))+0 -colorspace Gray "$d/2-${b}-$((x)).jpg"
-		convert "$f" -crop 50x40+$((x+25))+36 -colorspace Gray "$d/1-${b}-$((x+25)).jpg"
+		convert "$f" -crop 50x40+${x} -colorspace Gray "$d/130-${b}-${x}.jpg"
 	done
 done
