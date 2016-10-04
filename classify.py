db5e77bcf8b89d01bdcb1399319a0112df0b962c
Vladimir Eremin <veremin@mirantis.com>
2016-10-04 21:02:48 +0300

    Reporting

--- classify.py
+++ classify.py
@@ -11,17 +11,19 @@ except:
 
 import datetime
 import glob
+import logging
 import os.path
 import sys
 
 import numpy as np
 import skimage.io
-from skimage import exposure, img_as_float, img_as_ubyte
+from skimage import img_as_float, img_as_ubyte
 from skimage.color import rgb2gray
 from skimage.draw import line
+from skimage.exposure import equalize_adapthist
 from skimage.feature import canny
-from skimage.filters.rank import mean_bilateral
-from skimage.morphology import disk
+from skimage.morphology import label
+from skimage.restoration import denoise_bilateral
 from skimage.transform import rescale, ProjectiveTransform, warp
 from sklearn import svm
 from sklearn.externals import joblib
@@ -30,15 +32,16 @@ from sklearn.model_selection import GridSearchCV
 
 def adjust(img, tf, of, **kwargs):
     img = img_as_float(img)
-    img = exposure.equalize_adapthist(img, clip_limit=0.03)
+    img = equalize_adapthist(img, clip_limit=0.03)
     img = warp(img, tf, output_shape=of)
     return img
 
 
 def combine(imgs):
     img = np.mean(imgs, axis=0)
+    img = denoise_bilateral(img, multichannel=True)
     img = rgb2gray(img)
-    img = mean_bilateral(img, disk(20), s0=10, s1=10)
+    img = img_as_ubyte(img)
     return img
 
 
@@ -61,9 +64,9 @@ def get_probe(shape, image, x, y):
     """
     h, w = image.shape
     dx, dy = shape
-    x = x - max(min(x, 0), x+dx-h)
-    y = y - max(min(y, 0), y+dy-w)
-    return x, y, extract(image[x:x+dx, y:y+dy])
+    x = x - max(min(x, 0), x + dx - h)
+    y = y - max(min(y, 0), y + dy - w)
+    return x, y, extract(image[x:x + dx, y:y + dy])
 
 
 def load_knowndata(filenames, shape):
@@ -83,23 +86,23 @@ def load_knowndata(filenames, shape):
                 continue
             ty, tx, target = map(int, point.split('x'))
             tx, ty, probe = get_probe(shape, image, tx - dx / 2, ty - dy / 2)
-            print filename, tx, ty, target
+            logging.info('Datapoint: %s %d %d %d', filename, tx, ty, target)
             X.append(probe)
             y.append(target)
     return np.array(X), np.array(y)
 
 
 def train(filenames, shape):
-    Cs = np.logspace(-12, 3, 10)
+    Cs = np.logspace(-5, 5, 10)
     gammas = np.logspace(-8, -1, 8)
-    svc = svm.SVC(decision_function_shape='ovo')
+    svc = svm.SVC()
     clf = GridSearchCV(estimator=svc, param_grid=dict(C=Cs, gamma=gammas),
                        n_jobs=-1)
     X, y = load_knowndata(filenames, shape)
     clf.fit(X, y)
-    print clf.best_score_
-    print clf.best_estimator_.C
-    print clf.best_estimator_.gamma
+    logging.info('Model: %f C=%f gamma=%f', clf.best_score_,
+                 clf.best_estimator_.C,
+                 clf.best_estimator_.gamma)
     return clf.best_estimator_
 
 
@@ -110,8 +113,8 @@ def walk_image(image, shape, classifier):
 
     if classifier:
         h, w = image.shape
-        for x in range(0, h - dx + 1, dx / 5):
-            for y in range(0, w - dy + 1, dy / 5):
+        for x in range(0, h - dx + 1, dx / 3):
+            for y in range(0, w - dy + 1, dy / 3):
                 x, y, probe = get_probe(shape, image, x, y)
                 coords.append((x, y, dx, dy))
                 X.append(probe)
@@ -123,10 +126,8 @@ def walk_image(image, shape, classifier):
 
 
 def mark_region(image, x, y, h, w, mark=None):
-    if not mark:
-        return
-    image[line(x-1, y, x + h-1, y + w)] = 255-mark
-    image[line(x + h, y-1, x, y + w-1)] = 255-mark
+    image[line(x - 1, y, x + h - 1, y + w)] = 255 - mark
+    image[line(x + h, y - 1, x, y + w - 1)] = 255 - mark
     image[line(x, y, x + h, y + w)] = mark
     image[line(x + h, y, x, y + w)] = mark
 
@@ -149,22 +150,28 @@ def open_series(pattern, filter_func=lambda a: a, **kwargs):
     return series
 
 
+def calc_segments(arr, lpf):
+    labels = label(arr == 0)
+    bands = np.unique(labels, return_index=True, return_counts=True)
+    return [i for i in zip(bands[1], bands[2])[1:] if i[1] >= lpf]
+
+
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
     shape = dx, dy = 40, 40
     model = 'model.pkl'
     if os.path.exists(model):
         classifier = joblib.load(model)
-        print model
+        logging.info(model)
     else:
         filenames = sorted(glob.glob('train/*.jpg'))
         try:
             classifier = train(filenames, shape)
             joblib.dump(classifier, model)
+            logging.info(model)
         except Exception, e:
-            print e
+            logging.exception(e)
             classifier = None
-        print model
-
 
     of = (120, 680)
     tf = ProjectiveTransform()
@@ -175,10 +182,10 @@ if __name__ == '__main__':
         (of[1], 0)
     ))
     dst = np.array((
-       (117, 163),
-       (145, 279),
-       (613, 189),
-       (610, 135)
+        (117, 163),
+        (145, 279),
+        (613, 189),
+        (610, 135)
     ))
     tf.estimate(src, dst)
 
@@ -189,15 +196,21 @@ if __name__ == '__main__':
         img = open_series('test/*.jpg', adjust, tf=tf, of=of)
     img = combine(img)
 
-    #img = img_as_ubyte(img)
     coords, predicted = walk_image(img, shape, classifier)
     img_to_show = np.copy(img)
-
-    marks = []
+    holes = np.copy(img)
+    holes[0:of[0], 0:of[1]] = 0
     for coord, pred in zip(coords, predicted):
-        if pred > 128:
+        if pred > 127:
             x, y, dx, dy = coord
-            mark_region(img_to_show, x + dx/3, y + dy/3, dx/3*2, dy/3*2, pred)
+            mark_region(img_to_show, x + dx / 3, y + dy / 3, dx / 3 * 2,
+                        dy / 3 * 2, pred)
+            holes[x:x + dx, y:y + dy] = pred
+
+    for p in calc_segments(holes[90:91, 0:of[1]], 45):
+        print "%0.1f slot near %d place" % (float(p[1])/45, p[0]/40 + 1)
+    else:
+        print "No spaces"
 
     try:
         out = sys.argv[2]
@@ -206,7 +219,7 @@ if __name__ == '__main__':
 
     if out:
         skimage.io.imsave(out, img_to_show)
-        print out
+        logging.info(out)
     else:
         name = os.path.join("train", datetime.datetime.now().isoformat())
         log = None
@@ -220,8 +233,10 @@ if __name__ == '__main__':
                 except:
                     pass
                 log = open('{}.jpg.txt'.format(name), 'a+')
-                log.write('{}x{}x{}\n'.format(int(event.xdata), int(event.ydata),
-                                              int(255 / event.button)))
+                log.write(
+                    '{}x{}x{}\n'.format(int(event.xdata), int(event.ydata),
+                                        int(255 / event.button)))
+
 
         fig = plt.figure()
         ax = fig.add_subplot()
